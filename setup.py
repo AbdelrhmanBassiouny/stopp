5cd1e749e8628f98c76de487458f7db76aee4a2e
Bassio <Bassio@programmer.com>
2020-04-06 09:58:26 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'stopp',         # How you named your package folder (MyLib)
   packages = ['stopp'],   # Chose the same as "name"
-  version = '0.0',      # Start with a small number and increase it with every change you make
+  version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Safe Time Optimal Path Parametrization (STOPP) for serial robots',   # Give a short description about your library
   author = 'Abdelrhman Bassiouny',                   # Type in your name
